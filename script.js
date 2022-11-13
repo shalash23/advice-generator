69321f685bd90c5c990867c53ee6678c02c35bee
Mohamed Shalash <103309490+shalash23@users.noreply.github.com>
2022-11-13 15:55:37 +0200

    Updates

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const adviceNumber = document.querySelector(".advice-number");
 const advice = document.querySelector(".advice");
 const btn = document.querySelector(".icon-container");
+const svgDesktop = document.querySelector(".seperator-desktop");
+const svgMobile = document.querySelector(".seperator-mobile");
 
 const getAdvice = async function () {
   const response = await fetch("https://api.adviceslip.com/advice");
@@ -15,3 +17,19 @@ btn.addEventListener("click", async () => {
   adviceNumber.textContent = `Advice #${id}`;
   advice.textContent = `${advices}`;
 });
+
+const doit = () => {
+  const fn = () => {
+    if (window.innerWidth >= 578) {
+      svgMobile.classList.add("hidden");
+      svgDesktop.classList.remove("hidden");
+    }
+    if (window.innerWidth <= 578) {
+      svgMobile.classList.remove("hidden");
+      svgDesktop.classList.add("hidden");
+    }
+  };
+  fn();
+  window.addEventListener("resize", fn);
+};
+doit();
